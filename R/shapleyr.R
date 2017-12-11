d180fcf6697fe0a147c35b17169b3ec67634dfd3
hredich <hermann.redich@metoda.com>
2017-12-11 09:42:20 +0100

    hredich: first version implemented, not performant

--- R/shapleyr.R
+++ R/shapleyr.R
@@ -11,11 +11,34 @@
 #' @param data The dataset that contains x.
 #' @param algoirthm The algorithm that computes the expected value/class of x.
 #' @param method Determines how the shapley value is calculated. Possible
-#'   selections are "default" or "regression".
+#'   selections are "default" or "kernel".
 #' @return shapley value as a matrix with col.names and their corresponding
 #'   effects.
 #' @export
-shapley <- function(x, data, algorithm, method = "default") {
+shapley <- function(row.nr, task = bh.task, learner = "regr.lm",
+  iterations = 50, method = "default") {
 
-  return(x)
+  #FIXME: assert_factor(method, levels=c("default", "kernel"))
+  #FIXME: remove at least one loop with vectorized operations (apply)
+  mod = train(learner, task)
+  x = getTaskData(task)[row.nr,]
+  phi = as.data.frame(matrix(data = 0, nrow = 1, ncol = getTaskNFeats(task)))
+  names(phi) = getTaskFeatureNames(task)
+
+  for(feature in getTaskFeatureNames(task)) {
+    for(i in 1:iterations) {
+      z = getTaskData(task)[sample(getTaskSize(task), 1),]
+      perm = sample(getTaskFeatureNames(task))
+      position = match(feature, perm)
+
+      b1 = cbind(x[perm[1:position]], z[perm[min((position + 1), length(perm)):length(perm)]])
+      b2 = cbind(x[perm[1:max(1, (position - 1))]], z[perm[position:length(perm)]])
+
+      phi[feature] = phi[feature] +
+        getPredictionResponse(predict(mod, newdata = b1)) -
+        getPredictionResponse(predict(mod, newdata = b2))
+    }
+  }
+
+  return(phi / iterations)
 }
