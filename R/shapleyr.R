f35a249a2eab3ab274707db696b7e2d5870f26d7
hredich <hermann.redich@metoda.com>
2017-12-16 11:32:56 +0100

    add single point plot

--- R/shapleyr.R
+++ R/shapleyr.R
@@ -16,7 +16,7 @@
 #' @return shapley value as a data.frame with col.names and their corresponding
 #'   effects.
 #' @export
-shapley <- function(row.nr, task = bh.task, learner = "regr.lm",
+shapley = function(row.nr, task = bh.task, learner = "regr.lm",
   iterations = 50, method = "default") {
 
   #FIXME: assert_factor(method, levels=c("default", "kernel"))
@@ -44,3 +44,53 @@ shapley <- function(row.nr, task = bh.task, learner = "regr.lm",
 
   return(phi / iterations)
 }
+
+plot.shapley.singleValue = function(row.nr, shap.values, target = "medv",
+  task = bh.task, learner = "regr.lm") {
+
+  mod = train(learner, task)
+  pred =
+    getPredictionResponse(predict(mod, newdata = getTaskData(bh.task)[row.nr,]))
+
+  points = compute.shapley.positions(shap.values, pred)
+  ggplot(points, aes(x = values, y = 0, colour = values)) +
+    geom_line(size = 4) +
+    coord_cartesian(ylim = c(-0.4, 0.4)) +
+    scale_colour_gradientn(colours=rainbow(4)) +
+    geom_text(aes(label=names), size = 4, vjust = "top", hjust = points$align,
+      colour = "black", angle = 20) +
+    geom_point(aes(x = round(obs$medv, 3), y = 0.1), colour = "black", size = 3) +
+    theme(axis.title.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      legend.position = "none")
+}
+
+compute.shapley.positions = function(points, shift) {
+  points.minus = -1 * sort(points[which(points < 0)])
+  points.plus  = -1 * sort(points[which(points >= 0)], decreasing = TRUE)
+
+  for(i in 1:length(points.minus))
+    if(i > 1)
+      points.minus[i] = points.minus[i-1] + points.minus[i]
+  for(i in 1:length(points.plus))
+    if(i > 1)
+      points.plus[i] = points.plus[i-1] + points.plus[i]
+  positions = sort(cbind(points.minus, 0, points.plus))
+  result = data.frame(cbind(names(positions), t(round(positions + shift, 3))))
+  names(result) = c("names", "values")
+  result$values = sapply(sapply(result$values, as.character), as.numeric)
+  result$values = result$values
+  result$align = ifelse(result$values > shift,"right", "left")
+  rownames(result) = c()
+
+  return(result)
+}
+
+
+
+
+
+
+
+
