52e13f84143063b159f75dbe433c7581059e97fb
hredich <hermann.redich@metoda.com>
2018-01-19 15:33:32 +0100

    removed undefined method calls

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -10,30 +10,17 @@ test.covergence = function(row.nr, iterations = 20, shapley.iterations = 1) {
   return(values)
 }
 
-a = test.covergence(196, iterations = 60)
+a = test.covergence(196, iterations = 20)
 a.cum = cumsum(a) / seq_along(a)
 a.sub = a
 ggplot() +
   geom_point(aes(x = seq_along(a), y = a)) +
   geom_line(aes(x = seq_along(a), y = cumsum(a)/seq_along(a)), color = "red")
 
-a[5000]
-(cumsum(a) / seq_along(a))[4999]
-mean(a[60])
-
-
-# test what does the shapley really return...
-test.shapley = function() {
-  #do smthng
-}
-s.1 = shapley(196, iterations = 250)
-s.2 = shapley2(196, iterations = 250)
-
-system.time({ shapley(196, iterations = 1000)})
-system.time({ shapley2(196, iterations = 1000)})
+#system.time({ shapley(1:196, iterations = 30)})
 
 
 ## Für Daniel:
-for(i in 1:length(vec)) {
-  print(combn(vec, i))
-}
+#for(i in 1:length(vec)) {
+#  print(combn(vec, i))
+#}
