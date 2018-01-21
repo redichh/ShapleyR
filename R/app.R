9543614b754d93dbe5b12e13661e3f01233c695b
haijunXue <xuehaijun793g@gmail.com>
2018-01-21 23:30:25 +0100

    use shiny to display the shapley value

--- R/app.R
+++ R/app.R
@@ -1,5 +1,11 @@
-library(shiny)
-library(shinydashboard)
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 ui <- dashboardPage(
   dashboardHeader(title="shapley value"),
