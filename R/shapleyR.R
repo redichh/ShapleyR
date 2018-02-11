0dfa0a63d26d6cc1b69d3209e62e1459aa836884
daniel.biman <daniel.biman@mail.de>
2018-02-11 15:58:38 +0100

    Added assert functions to check arguments of shapley function.

--- R/shapleyR.R
+++ R/shapleyR.R
@@ -20,11 +20,16 @@ shapley = function(row.nr, model = train("regr.lm", bh.task), task = bh.task,
   iterations = 50, method = "default") {
 
   #FIXME: add version with unsampled permutation for small feature vectors
-  #FIXME: assert_numeric(row.nr, nmin = 1)/assert_class(model, "WrappedModel")
   #FIXME: Packages from DESCRIPTION.Imports are not imported correctly...
-  #FIXME: add further methods = c("default", "kernel", "exact", "hayvan-dividente"))
+  #FIXME: add further methods = c("default", "kernel", "exact", "harsanyi-dividends"))
   #FIXME: test/implement further task kinds (classification, clustering)
   #FIXME: add "#' @importFrom mlr train" for methods
+
+
+  assert_int(row.nr, lower = 1)
+  assert_int(iterations, lower = 1)
+  assert_class(model, "WrappedModel")
+
 
   x = getTaskData(task)[row.nr,]
   phi = as.data.frame(matrix(data = 0, nrow = nrow(x) * iterations, ncol = getTaskNFeats(task)))
