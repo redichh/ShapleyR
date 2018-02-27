f34fe6b34929950ce602fcba9ef07e22a5286952
daniel.biman <daniel.biman@mail.de>
2018-02-27 22:42:52 +0100

    Test data with other column names In Shapley unsampled (new version): Works now with every column name in test data no warnings anymore output is a data frame

--- R/test_data_exact_shapley.R
+++ R/test_data_exact_shapley.R
@@ -1,7 +1,3 @@
-#Exakter Shapley wert
-#Folgende Formel umsetzen:
-# Sh_i(N,v) = 1/n! sum[ v(P_i u {i}) - v(P_i) ]
-
 test = as.data.frame(rbind(c(1,0,0,12),
   c(0,1,0,6),
   c(0,0,1,9),
@@ -10,4 +6,4 @@ test = as.data.frame(rbind(c(1,0,0,12),
   c(0,1,1,15),
   c(1,1,1,36)))
 
-names(test) = c("A", "B", "C", "value")
+names(test) = c("Alter", "Zahl1", "ALg", "value")
--- R/test_shapley_unsampl.R
+++ R/test_shapley_unsampl.R
@@ -1,3 +1,59 @@
+#######New version number 3
+#No warnings anymore
+#Output as data frame
+#works with every column name of data.input
+#Shapley value calculated by shapley.calc
+#Iterates over all player
+shapley.unsampled = function(data.input = test, target = "value") {
+  #assert()
+  #df is a list without target variable
+  df = data.input[, !(names(data.input) %in% target)]
+  #Change names(df) to letters, so sort() works for all kind of names
+  players = LETTERS[1:length(names(df))]
+  #Find all permutations of players
+  S = combinat::permn(players)
+
+  shapley.calc = function(observed, S){
+    sh.diff = c()
+    for(i in 1:length(S)){
+      index = grep(pattern = observed, x=S[[i]])
+      #Choose players before/with observed player, No coalation = NA
+      player.with = c(S[[i]][1:index])
+      if(index!=1){
+        player.before = c(S[[i]][1:(index-1)])
+      }
+      else player.before = NA
+      #Target value of coalations
+      for(j in 1:nrow(df)){
+        curr.coal = c(players[df[j,] == 1])
+        if(length(curr.coal)==length(player.with) && all(sort(curr.coal)==sort(player.with))){
+          target.with = data.input$value[j]
+        }
+        if(length(player.before)==1 && is.na(player.before)){
+          target.before = 0
+        }
+        if(length(curr.coal)==length(player.before) && all(sort(curr.coal)==sort(player.before))){
+          target.before = data.input$value[j]
+        }
+      }
+      #Substract the value of coalations
+      sh.diff = append(sh.diff, target.with - target.before)
+    }
+    return(mean(sh.diff))
+  }
+  ##iterate observed player
+  sh.all = as.data.frame(matrix(data = 0, ncol = length(names(df)), nrow = 1))
+  names(sh.all) = names(df)
+  for (player in players){
+    shapley.value = shapley.calc(observed = player, S = S)
+    column.index = grep(pattern = player, x=players) #from letters back to original column names
+    sh.all[ ,column.index] = shapley.value
+  }
+  return(sh.all)
+}
+
+#######
+#######First version
 #Shapley value calculatet by shapley.calc
 #Iterates over all player
 shapley.unsampled = function(data.input = test, target = "value") {
@@ -13,7 +69,9 @@ shapley.unsampled = function(data.input = test, target = "value") {
       index = grep(pattern = observed, x=S[[i]])
       #Choose players before/with observed player, No coalation = NA
       player.with = c(S[[i]][1:index])
-      if(index!=1) player.before = c(S[[i]][1:(index-1)])
+      if(index!=1){
+        player.before = c(S[[i]][1:(index-1)])
+      }
       else player.before = NA
       #Target value of coalations
       for(j in 1:nrow(df)){
@@ -41,7 +99,6 @@ shapley.unsampled = function(data.input = test, target = "value") {
   }
   return(sh.all)
 }
-print(sh.all)
 
 #######
 #######Second, older version.No iteration through player. No shapley.calc function.
@@ -56,7 +113,9 @@ shapley.unsampled = function(data.input = test, target = "value",  observed = "B
     index = grep(pattern = observed, x=S[[i]])
     #Choose players before/with observed player, No coalation = NA
     player.with = c(S[[i]][1:index])
-    if(index!=1) player.before = c(S[[i]][1:(index-1)])
+    if(index!=1){
+      player.before = c(S[[i]][1:(index-1)])
+    }
     else player.before = NA
     #Target value of coalations
     for(j in 1:nrow(df)){
@@ -76,3 +135,5 @@ shapley.unsampled = function(data.input = test, target = "value",  observed = "B
   }
   return(mean(sh))
 }
+
+
