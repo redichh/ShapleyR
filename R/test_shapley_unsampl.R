71bf6e2054d3ce3bd9498ef603ff41ab3032560d
daniel.biman <daniel.biman@mail.de>
2018-03-06 18:24:13 +0100

    some small style corrections deleted previous versions PS: untill now, the shapley.calc function must be in the shapley.unsampled function

--- R/test_shapley_unsampl.R
+++ R/test_shapley_unsampl.R
@@ -1,34 +1,38 @@
-#######New version number 4
-#add defensive programming
-#add apply instead of for loops
+#######New version number 5
+#some style correction
+#deleted previous versions
 
+##Asserts, Permutation and iteration through player
 shapley.unsampled = function(data.input = test, target = "value") {
   ##Assert
   assert_data_frame(data.input)
-  assert_numeric(data.input[ ,(names(data.input) %in% target)])
+  assert_numeric(data.input[,target])
 
-  df = data.input[ ,!(names(data.input) %in% target)] #coalations without target variable
-  stopifnot(all(sapply(df, function(X) X == FALSE | X == TRUE))) #Only logical in df
+  df = data.input[,!(names(data.input) %in% target)] #coalations without target variable
+  stopifnot(all(sapply(df, function(x) x == FALSE | x == TRUE))) #Only logical in df
+  #Check expected row number (with/without empty coalation)
   if(0 %in% rowSums(df)){
-    assert_data_frame(df, nrows = 2^ncol(df)) #Check expected row number (with/without empty coalation)
+    assert_data_frame(df, nrows = 2^ncol(df))
   }
-  else assert_data_frame(df, nrows = 2^ncol(df)-1)
+  else
+    assert_data_frame(df, nrows = 2^ncol(df)-1)
   stopifnot(identical(df, unique(df))) #Check uniqueness = every coalation once in data
 
   ##Permutations
-  players = LETTERS[1:ncol(df)] #Change names of columns to letters, so sort() works for any name
+  players = paste0("V", seq(1,ncol(df))) #Change names of columns to letters, so sort() works for any name
   S = combinat::permn(players) #Find all permutations of players
   ##Calculate Shapley
   shapley.calc = function(observed, S){
     sh.diff = c()
     for(i in 1:length(S)){
-      index = grep(pattern = observed, x=S[[i]])
+      index = grep(pattern = observed, x = S[[i]])
       #Choose players before/with observed player, No coalation = NA
       player.with = c(S[[i]][1:index])
-      if(index!=1){
+      if(index != 1){
         player.before = c(S[[i]][1:(index-1)])
       }
-      else player.before = NA
+      else
+        player.before = NA
       #Target value of coalations
       for(j in 1:nrow(df)){
         curr.coal = c(players[df[j,] == 1])
@@ -52,95 +56,8 @@ shapley.unsampled = function(data.input = test, target = "value") {
   sh.all = as.data.frame(matrix(data = 0, ncol = ncol(df), nrow = 1))
   names(sh.all) = names(df)
   for (player in players){
-    shapley.value = shapley.calc(observed = player, S = S)
     column.index = grep(pattern = player, x = players) #from letters back to original column names
-    sh.all[ ,column.index] = shapley.value
+    sh.all[,column.index] = shapley.calc(observed = player, S = S)
   }
   return(sh.all)
 }
-
-#######
-#######First version
-#Shapley value calculatet by shapley.calc
-#Iterates over all player
-shapley.unsampled = function(data.input = test, target = "value") {
-  #assert()
-  #df is a list without target variable
-  df = data.input[, !(names(data.input) %in% target)]
-  #Find all permutations of players
-  S = combinat::permn(names(df))
-
-  shapley.calc = function(observed, S){
-    sh.diff = c()
-    for(i in 1:length(S)){
-      index = grep(pattern = observed, x=S[[i]])
-      #Choose players before/with observed player, No coalation = NA
-      player.with = c(S[[i]][1:index])
-      if(index!=1){
-        player.before = c(S[[i]][1:(index-1)])
-      }
-      else player.before = NA
-      #Target value of coalations
-      for(j in 1:nrow(df)){
-        curr.coal = c(names(df)[df[j,] == 1])
-        if(length(curr.coal)==length(player.with) & all(sort(curr.coal)==sort(player.with))){
-          target.with = data.input$value[j]
-        }
-        if(is.na(player.before)){
-          target.before = 0
-        }
-        if(length(curr.coal)==length(player.before) & all(sort(curr.coal)==sort(player.before))){
-          target.before = data.input$value[j]
-        }
-      }
-      #Substract the value of coalations
-      sh.diff = append(sh.diff, target.with - target.before)
-    }
-    return(mean(sh.diff))
-  }
-  ##iterate observed player
-  sh.all = c()
-  for (observed in names(df)){
-    shapley.value = shapley.calc(observed = observed, S = S)
-    sh.all = append(sh.all, paste(observed, shapley.value))
-  }
-  return(sh.all)
-}
-
-#######
-#######Second, older version.No iteration through player. No shapley.calc function.
-sh = c()
-shapley.unsampled = function(data.input = test, target = "value",  observed = "B") {
-  #assert()
-  #df is a list without target variable
-  df = data.input[, !(names(data.input) %in% target)]
-  #Find all permutations of players
-  S = combinat::permn(names(df))
-  for(i in 1:length(S)){
-    index = grep(pattern = observed, x=S[[i]])
-    #Choose players before/with observed player, No coalation = NA
-    player.with = c(S[[i]][1:index])
-    if(index!=1){
-      player.before = c(S[[i]][1:(index-1)])
-    }
-    else player.before = NA
-    #Target value of coalations
-    for(j in 1:nrow(df)){
-      curr.coal = c(names(df)[df[j,] == 1])
-      if(length(curr.coal)==length(player.with) & all(sort(curr.coal)==sort(player.with))){
-        target.with = data.input$value[j]
-      }
-      if(is.na(player.before)){
-        target.before = 0
-      }
-      if(length(curr.coal)==length(player.before) & all(sort(curr.coal)==sort(player.before))){
-        target.before = data.input$value[j]
-      }
-    }
-    #Substract the value of coalations
-    sh = append(sh, target.with - target.before)
-  }
-  return(mean(sh))
-}
-
-
