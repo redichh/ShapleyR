d2ad76734d3379fabca9ae05a538b5174295eb98
hredich <hermann.redich@metoda.com>
2018-03-13 18:14:42 +0100

    add classification/clustering for predict.type='prob'

--- R/shapleyR.R
+++ R/shapleyR.R
@@ -19,7 +19,6 @@
 shapley = function(row.nr, learner = makeLearner("regr.lm"), task = bh.task,
   model = train(learner, bh.task), iterations = 50, method = "regr") {
 
-  #FIXME: implement further task kinds (clustering, decision trees)
   assert_numeric(row.nr, min.len = 1, lower = 1, upper = nrow(getTaskData(task)))
   assert_int(iterations, lower = 1)
   assert_class(model, "WrappedModel")
@@ -29,20 +28,21 @@ shapley = function(row.nr, learner = makeLearner("regr.lm"), task = bh.task,
     result = shapley.regr(row.nr, model, task, iterations)
   } else if (method == "classif") {
     result = shapley.classif(row.nr, learner, task, model, iterations)
+  } else if (method == "clustering") {
+    result = shapley.clustering(row.nr, learner, task, model, iterations)
   } else if (method == "unsampled") {
     print("Work in progress")
+    #FIXME: implement unsampled version
     # result = shapley.unsampled()
   } else {
-    print(paste0("Method: '", method, "' not supported"))
+    print(paste0("Method: '", method, "' not supported!"))
   }
 
   return(result)
 }
 
 
-shapley.regr = function(row.nr, task = bh.task, model = train("regr.lm", bh.task),
-  iterations = 50) {
-
+shapley.regr = function(row.nr, task = bh.task, model = train("regr.lm", bh.task), iterations = 50) {
   assert_set_equal(getTaskType(task), "regr")
 
   x = getTaskData(task)[row.nr,]
@@ -92,7 +92,54 @@ shapley.classif = function(row.nr, learner = makeLearner("classif.lda", predict.
 
   task.levels = getTaskClassLevels(task)
   custom.names = c("_Id", "_Class")
-  result = as.data.frame(matrix(data = 0, ncol = ncol(phi) + length(custom.names),
+  result = as.data.frame(matrix(data = 0, ncol = getTaskNFeats(task) + length(custom.names),
+    nrow = nrow(x) * length(task.levels)))
+  names(result) = c(custom.names, names(b1))
+  result$"_Id" = sort(rep(row.nr, times = length(task.levels)))
+  result$"_Class" = rep(task.levels, times = nrow(x))
+
+  for(feature in getTaskFeatureNames(task)) {
+    for(i in 1:iterations) {
+      z = getTaskData(task)[sample(getTaskSize(task), nrow(x)),]
+      perm = sample(getTaskFeatureNames(task))
+      position = match(feature, perm)
+
+      s = (i - 1) * nrow(x) + 1
+      prec = if(position == 1) NULL else perm[1:(position - 1)]
+      succ = if(position == length(perm)) NULL else perm[(position + 1):length(perm)]
+      b1[s:(s + nrow(x) - 1), perm] = cbind(x[prec], x[feature], z[succ])
+      b2[s:(s + nrow(x) - 1), perm] = cbind(x[prec], z[feature], z[succ])
+    }
+
+    predict_b1 = getPredictionProbabilities(predict(model, newdata=b1), getTaskClassLevels(task))
+    predict_b2 = getPredictionProbabilities(predict(model, newdata=b2), getTaskClassLevels(task))
+    nclasses = length(getTaskClassLevels(task))
+    for(i in 1:nrow(x)) {
+      r.indices = nclasses * (i - 1) + 1:nclasses
+      p.indices = iterations * (i - 1) + 1:iterations
+      result[r.indices, feature] =
+        round(colMeans(predict_b1[p.indices,] - predict_b2[p.indices,]), 3)
+    }
+  }
+
+  return(result)
+}
+
+
+shapley.clustering = function(row.nr, learner = makeLearner("cluster.kmeans", predict.type = "prob"),
+  task = mtcars.task, model = train(learner, task), iterations = 50) {
+
+  assert_set_equal(getTaskType(task), "cluster")
+  assert_set_equal(getLearnerPredictType(learner), "prob")
+
+  x = getTaskData(task)[row.nr,]
+  b1 = data.frame(subset(x, select = getTaskFeatureNames(task)))
+  b1[1:(nrow(x) * iterations),] = NA
+  b2 = b1
+
+  task.levels = seq(1, model$learner$par.vals$centers)
+  custom.names = c("_Id", "_Class")
+  result = as.data.frame(matrix(data = 0, ncol = getTaskNFeats(task) + length(custom.names),
     nrow = nrow(x) * length(task.levels)))
   names(result) = c(custom.names, names(b1))
   result$"_Id" = sort(rep(row.nr, times = length(task.levels)))
@@ -113,7 +160,7 @@ shapley.classif = function(row.nr, learner = makeLearner("classif.lda", predict.
 
     predict_b1 = getPredictionProbabilities(predict(model, newdata=b1))
     predict_b2 = getPredictionProbabilities(predict(model, newdata=b2))
-    nclasses = length(getTaskClassLevels(task))
+    nclasses = model$learner$par.vals$centers
     for(i in 1:nrow(x)) {
       r.indices = nclasses * (i - 1) + 1:nclasses
       p.indices = iterations * (i - 1) + 1:iterations
