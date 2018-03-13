43ce0ac3b530dad816672215012e6a968ad065a9
hredich <hermann.redich@metoda.com>
2018-03-13 18:21:16 +0100

    new handling with 'shapley....'-subfunctions

--- R/shapleyR.R
+++ R/shapleyR.R
@@ -16,20 +16,16 @@
 #' @return shapley value as a data.frame with col.names and their corresponding
 #'   effects.
 #' @export
-shapley = function(row.nr, model = train("regr.lm", bh.task), task = bh.task,
-  iterations = 50, method = "default") {
-
+shapley = function(row.nr, task = bh.task, model = train("regr.lm", bh.task), iterations = 50) {
   assert_numeric(row.nr, min.len = 1, lower = 1, upper = nrow(getTaskData(task)))
   assert_int(iterations, lower = 1)
   assert_class(model, "WrappedModel")
 
   x = getTaskData(task)[row.nr,]
-  phi = as.data.frame(matrix(data = 0, nrow = nrow(x) * iterations, ncol = getTaskNFeats(task)))
-  names(phi) = getTaskFeatureNames(task)
-
-  b1 = data.frame(subset(x, select = names(phi)))
+  b1 = data.frame(subset(x, select = getTaskFeatureNames(task)))
   b1[1:(nrow(x) * iterations),] = NA
   b2 = b1
+  result = prepareResult(x, task, model)
 
   for(feature in getTaskFeatureNames(task)) {
     for(i in 1:iterations) {
@@ -44,14 +40,59 @@ shapley = function(row.nr, model = train("regr.lm", bh.task), task = bh.task,
       b2[s:(s + nrow(x) - 1), perm] = cbind(x[prec], z[feature], z[succ])
     }
 
-    phi[feature] = getPredictionResponse(predict(model, newdata=b1)) -
-      getPredictionResponse(predict(model, newdata = b2))
+    predict_b1 = getPredictionData(b1, model, task)
+    predict_b2 = getPredictionData(b2, model, task)
+    nclasses = 1
+    if(getTaskType(task) == "classif")
+      nclasses = length(getTaskClassLevels(task))
+    if(getTaskType(task) == "cluster")
+      nclasses = model$learner$par.vals$centers
+
+    for(i in 1:nrow(x)) {
+      r.indices = nclasses * (i - 1) + 1:nclasses
+      p.indices = iterations * (i - 1) + 1:iterations
+      result[r.indices, feature] = computePartialResult(predict_b1, predict_b2, p.indices, getTaskType(task))
+    }
   }
 
-  result = as.data.frame(matrix(data=0, ncol=ncol(phi), nrow=nrow(x)))
-  names(result) = names(b1)
-  for(i in 1:nrow(x))
-    result[i,] = colMeans(phi[seq(from=i, to=nrow(phi), by=nrow(x)), ])
+  return(result)
+}
 
-  return(round(result, 3))
+getPredictionData = function(data, model, task) {
+  result = NA
+  if(getTaskType(task) == "regr") {
+    result = as.data.frame(getPredictionResponse(predict(model, newdata=data)))
+  } else if(getTaskType(task) == "classif") {
+    result = getPredictionProbabilities(predict(model, newdata=data), getTaskClassLevels(task))
+  } else if(getTaskType(task) == "cluster") {
+    result = getPredictionProbabilities(predict(model, newdata=data))
+  }
+
+  return(result)
+}
+
+prepareResult = function(x, task, model) {
+  task.levels = NA
+  if(getTaskType(task) == "classif")
+    task.levels = getTaskClassLevels(task)
+  else if(getTaskType(task) == "cluster")
+    task.levels = seq(1, model$learner$par.vals$centers)
+  custom.names = c("_Id", "_Class")
+  result = as.data.frame(matrix(data = 0, ncol = getTaskNFeats(task) + length(custom.names),
+    nrow = nrow(x) * length(task.levels)))
+  names(result) = c(custom.names, getTaskFeatureNames(task))
+  result$"_Id" = sort(rep(row.names(x), times = length(task.levels)))
+  result$"_Class" = rep(task.levels, times = nrow(x))
+
+  return(result)
+}
+
+computePartialResult = function(predict_a, predict_b, indices, task_type) {
+  result = predict_a[indices,] - predict_b[indices,]
+  if(task_type == "regr")
+    result = round(mean(result), 3)
+  else
+    result = round(colMeans(result), 3)
+
+  return(result)
 }
