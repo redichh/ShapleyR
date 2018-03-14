e4efc3d9ad5ce3bc854035d806a278860d27049f
daniel.biman <daniel.biman@mail.de>
2018-03-14 22:26:44 +0100

    added regression test task and empty coalation

--- R/test_data_exact_shapley.R
+++ R/test_data_exact_shapley.R
@@ -4,6 +4,9 @@ test = as.data.frame(rbind(c(1,0,0,12),
   c(1,1,0,24),
   c(1,0,1,27),
   c(0,1,1,15),
+  c(0,0,0,0),
   c(1,1,1,36)))
 
-names(test) = c("Alter", "Zahl1", "ALg", "value")
+names(test) = c("Ha", "He", "Da", "value")
+
+test.task = makeRegrTask(id = "wiki-example", data = test, target = "value")
