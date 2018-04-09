5c7111577ddac52a77b105289f07b2ab683f0a89
daniel.biman <daniel.biman@mail.de>
2018-04-09 18:02:30 +0200

    Removed dependencies on other packages. Added description of parameter.

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -4,6 +4,7 @@
 #' @param row.nr Index for the observation of interest.
 #' @param convergence.iterations Amount of calls of the shapley function.
 #' @param iterations Amount of the iterations within the shapley function.
+#' @param return.value You can choose between plotting results or getting a data frame
 #' @return shapley values as a data.frame or a plot
 #'
 test.convergence = function(row.nr=2, convergence.iterations = 20, iterations = 20, task = iris.task,
@@ -16,16 +17,16 @@ test.convergence = function(row.nr=2, convergence.iterations = 20, iterations = 
 
   data = getTaskData(task)[row.nr,]
   prediction = getPredictionData(data, model, task)
-  prediction.class = names(prediction[BBmisc::getMaxIndex(prediction)])
+  prediction.class = names(prediction[match(max(prediction), prediction)])
 
   values = rep(0, times = convergence.iterations)
   for(i in 1:convergence.iterations) {
+    shap = shapley(row.nr, task, model, iterations)
     if(getTaskType(task) == "classif"){
-      shap = shapley(row.nr, task, model, iterations)
-      values[i] = shap$"_Class"[BBmisc::getMaxIndex(rowSums(shap[,getTaskFeatureNames(task)]))]
+      shap.sum = rowSums(shap[,getTaskFeatureNames(task)])
+      values[i] = shap$"_Class"[match(max(shap.sum), shap.sum)]
     }
     else if(getTaskType(task) == "regr"){
-      shap = shapley(row.nr, task, model, iterations)
       truth = getPredictionTruth(prediction)
       values[i] = sum(shap[,getTaskFeatureNames(task)]) + truth
     }
@@ -41,9 +42,10 @@ test.convergence = function(row.nr=2, convergence.iterations = 20, iterations = 
   }
   if(return.value == "values") {
     if(getTaskType(task) == "classif"){
-      result = plyr::count(as.data.frame(values))
-      result$values = as.character(result$values)
-      result$values[match(prediction.class, result$values)] = paste0(">>", prediction.class, "<<")
+      values = replace(values, grep(prediction.class, values), paste0(">>", prediction.class, "<<"))
+      result = as.data.frame(values)
+      names(result) = "values"
+      result = summary(result$values)
     }
     else if(getTaskType(task) == "regr"){
       result = as.data.frame(values)
