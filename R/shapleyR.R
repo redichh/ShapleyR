884bb2e32547621f5642cc71c20e6f20d5daca84
redichh <33175541+redichh@users.noreply.github.com>
2018-04-10 10:51:07 +0200

    add multilabel; started performance+ (#12)

--- R/shapleyR.R
+++ R/shapleyR.R
@@ -11,47 +11,54 @@
 #' @param task mlr task that contains the data set.
 #' @param model Model for the corresponding task..
 #' @param iterations Amount of iterations.
-#' @param method Determines how the shapley value is calculated. Possible
-#'   methods are "default" or "kernel".
 #' @return shapley value as a data.frame with col.names and their corresponding
 #'   effects.
 #' @export
-shapley = function(row.nr, task = mtcars.task, model = train(makeLearner("cluster.kmeans", predict.type = "response"), mtcars.task), iterations = 50) {
+shapley = function(row.nr, task = bh.task, model = train(makeLearner("regr.lm"), bh.task), iterations = 100) {
   assert_numeric(row.nr, min.len = 1, lower = 1, upper = nrow(getTaskData(task)))
   assert_int(iterations, lower = 1)
   assert_class(model, "WrappedModel")
 
   x = getTaskData(task)[row.nr,]
-  b1 = data.frame(subset(x, select = getTaskFeatureNames(task)))
-  b1[1:(nrow(x) * iterations),] = NA
+  f.indices = nrow(x) * iterations
+  feature.names = getTaskFeatureNames(task)
+  task.type = getTaskType(task)
+  b1 = data.frame(subset(x, select = feature.names))
+  b1[1:(f.indices * length(feature.names)),] = NA
   b2 = b1
   result = prepareResult(x, task, model)
 
-  for(feature in getTaskFeatureNames(task)) {
+  for(f in 1:length(feature.names)) {
+    feature = feature.names[f]
     for(i in 1:iterations) {
       z = getTaskData(task)[sample(getTaskSize(task), nrow(x)),]
-      perm = sample(getTaskFeatureNames(task))
+      perm = sample(feature.names)
       position = match(feature, perm)
 
-      s = (i - 1) * nrow(x) + 1
+      s = (f - 1) * f.indices + (i - 1) * nrow(x) + 1
       prec = if(position == 1) NULL else perm[1:(position - 1)]
       succ = if(position == length(perm)) NULL else perm[(position + 1):length(perm)]
       b1[s:(s + nrow(x) - 1), perm] = cbind(x[prec], x[feature], z[succ])
       b2[s:(s + nrow(x) - 1), perm] = cbind(x[prec], z[feature], z[succ])
     }
+  }
 
-    predict_b1 = getPredictionData(b1, model, task)
-    predict_b2 = getPredictionData(b2, model, task)
-    nclasses = 1
-    if(getTaskType(task) == "classif")
-      nclasses = length(getTaskClassLevels(task))
-    if(getTaskType(task) == "cluster")
-      nclasses = model$learner$par.vals$centers
+  nclasses = 1
+  if(task.type %in% c("classif", "multilabel"))
+    nclasses = length(getTaskClassLevels(task))
+  if(task.type == "cluster")
+    nclasses = model$learner$par.vals$centers
+  predict_b1 = getPredictionData(b1, model, task)
+  predict_b2 = getPredictionData(b2, model, task)
 
+  for(f in 1:length(feature.names)) {
     for(i in 1:nrow(x)) {
       r.indices = nclasses * (i - 1) + 1:nclasses
-      p.indices = ifelse(getLearnerPredictType(model$learner) == "response", 1, iterations * (i - 1) + 1:iterations)
-      result[r.indices, feature] = computePartialResult(predict_b1, predict_b2, p.indices, getTaskType(task))
+      s = (f - 1) * f.indices + (i - 1) * nrow(x) + 1
+      p.indices = ifelse(getLearnerPredictType(model$learner) == "response",
+        s:(s + nrow(x) - 1), (f - 1) * f.indices + iterations * (i - 1) + 1:iterations)
+      result[r.indices, feature.names[f]] =
+        computePartialResult(predict_b1, predict_b2, p.indices, task.type)
     }
   }
 
@@ -60,22 +67,20 @@ shapley = function(row.nr, task = mtcars.task, model = train(makeLearner("cluste
 
 getPredictionData = function(data, model, task) {
   result = NA
-
-  if(getLearnerPredictType(model$learner) == "response" & (getTaskType(task) == "classif" | getTaskType(task) == "cluster")) {
-    task.levels = NA
-    if(getTaskType(task) == "classif") {
-      task.levels = getTaskClassLevels(task)
-    } else {
-      task.levels = seq(1, model$learner$par.vals$centers, by = 1)
-    }
+  response.types = c("classif", "cluster", "multilabel")
+  if(getLearnerPredictType(model$learner) == "response" &  getTaskType(task) %in% response.types) {
+    task.levels = custom.ifelse(getTaskType(task) == "cluster",
+      seq(1, model$learner$par.vals$centers, by = 1), getTaskClassLevels(task))
     response = getPredictionResponse(predict(model, newdata=data))
     estimated = as.data.frame(matrix(data = 0, nrow = 1, ncol = length(task.levels)))
     observed = as.data.frame(matrix(data = 0, nrow = nrow(data), ncol = length(task.levels)))
     names(estimated) = task.levels
     names(observed) = task.levels
     for(i in 1:nrow(observed)) {
-      estimated[1, as.character(response[i])] = estimated[1, as.character(response[i])] + (1 / nrow(data))
-      observed[i, as.character(response[i])] = 1
+      col.indices = custom.ifelse(getTaskType(task) == "multilabel",
+        colnames(response)[response[i,]], as.character(response[i]))
+      estimated[1, col.indices] = estimated[1, col.indices] + (1 / nrow(data))
+      observed[i, col.indices] = 1
     }
     result = observed
     for(i in 1:nrow(observed))
@@ -93,15 +98,16 @@ getPredictionData = function(data, model, task) {
 
 prepareResult = function(x, task, model) {
   task.levels = NA
-  if(getTaskType(task) == "classif")
+  if(getTaskType(task) %in% c("classif", "multilabel")) {
     task.levels = getTaskClassLevels(task)
-  else if(getTaskType(task) == "cluster")
+  } else if(getTaskType(task) == "cluster") {
     task.levels = seq(1, model$learner$par.vals$centers)
+  }
   custom.names = c("_Id", "_Class")
   result = as.data.frame(matrix(data = 0, ncol = getTaskNFeats(task) + length(custom.names),
     nrow = nrow(x) * length(task.levels)))
   names(result) = c(custom.names, getTaskFeatureNames(task))
-  result$"_Id" = sort(rep(row.names(x), times = length(task.levels)))
+  result$"_Id" = sort(rep(as.numeric(row.names(x)), times = length(task.levels)))
   result$"_Class" = rep(task.levels, times = nrow(x))
 
   return(result)
@@ -115,4 +121,11 @@ computePartialResult = function(predict_a, predict_b, indices, task_type) {
     result = round(colMeans(result), 3)
 
   return(result)
+}
+
+custom.ifelse = function(condition, then.case, else.case) {
+  if(condition)
+    return(then.case)
+  else
+    return(else.case)
 }
